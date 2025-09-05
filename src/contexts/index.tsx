5c9844a08104695cccaae2c9293490b319b19ba5
Severino Carvalho <severinocarvalho14@gmail.com>
2025-09-04 23:55:24 -0300

    SEP-6: Removendo develop stats

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,9 +1,7 @@
 import { queryClient } from '@/lib/useQuery/query-client'
 import { IChildren } from '@/types/components/IBasicComponent'
 import { LOCAL_STORAGE_ENUM } from '@/types/enums/local-storage-key-enum'
-import { SEP_ENVIRONMENT } from '@/types/envs'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthProvider } from './providers/auth-provider'
 import { TemaProvider } from './providers/tema-provider'
 
@@ -18,8 +16,6 @@ export function Contexts({ children }: Readonly<IChildren>) {
 					{children}
 				</TemaProvider>
 			</AuthProvider>
-
-			{SEP_ENVIRONMENT === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
 		</QueryClientProvider>
 	)
 }
